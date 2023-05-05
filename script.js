bc4759b3f660ea5b0ec31bff7a14cc77fda10e11
Dave Nash <davidjnash2@gmail.com>
2023-05-05 00:02:43 -0500

    created index file, javascript file, and css file, and imported jQuery, and linked all together

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('yo!');
+$(document).ready(onReady); // added the first line load (boilerplate, it seems?)
+
+function onReady() { // declare onReady function
+    console.log(2);
+}
+
+console.log(1)
