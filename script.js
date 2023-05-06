189983daf1a5767ae6f0e20dd0e2193b4c59ab7d
Dave Nash <davidjnash2@gmail.com>
2023-05-06 01:42:14 -0500

    attempting to integrate form fields within table, for better appearance, and added handleSubmit event handler and started corresponding function

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-console.log('yo!');
-$(document).ready(onReady); // added the first line load (boilerplate, it seems?)
+// console.log('yo!');
+$(document).ready(onReady); // get ready
 
 function onReady() { // declare onReady function
-    console.log(2);
+    // console.log(2);
+
+    $('.submit-button').on('click', handleSubmit);
+
 }
 
-console.log(1)
+// console.log(1)
+
+function handleSubmit() {
+    // console.log('are we there yet?')
+    
+    let firstNameInput=$('#firstNameInput').val();
+    let lastNameInput=$('#lastNameInput').val();
+    let employeeIdInput=$('#employeeIdInput').val();
+    let jobTitleInput=$('#jobTitleInput').val();
+    let annualSalaryInput=$('#annualSalaryInput').val();
+    console.log(firstNameInput);
+    console.log(lastNameInput);
+    console.log(employeeIdInput);
+    console.log(jobTitleInput);
+    console.log(annualSalaryInput);
+
+
+    event.preventDefault();
+    $('input').val('');
+
+    $('tbody').append(`
+        <tr>
+        <td>${firstNameInput}</td>
+        <td>${lastNameInput}</td>
+        <td>${employeeIdInput}</td>
+        <td>${jobTitleInput}</td>
+        <td>${annualSalaryInput}</td>
+        <td><button class="deleteMe">❌🍖</button></td>
+        </tr>`)
+}
