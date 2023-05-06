fdd28711864bc552f059e3ba497cde2917f0e267
Dave Nash <davidjnash2@gmail.com>
2023-05-06 13:22:15 -0500

    turned off autocomplete via markdown because it was causing undesired floating dialogue in chrome, which seemed to fix to my desired interface result, and also updated handleSubmit function in js with conditional so that empty imputs will not capture, and submit button will only append if all form inputs are filled

--- script.js
+++ script.js
@@ -14,33 +14,44 @@ function onReady() { // declare onReady function
 
 function handleSubmit() { // declare handle submit function
     // console.log('are we there yet?') // log to test
-    
+
     let firstNameInput=$('#firstNameInput').val(); // link input variable to form input
     let lastNameInput=$('#lastNameInput').val(); // link input variable to form input
     let employeeIdInput=$('#employeeIdInput').val(); // link input variable to form input
     let jobTitleInput=$('#jobTitleInput').val(); // link input variable to form input
     let annualSalaryInput=$('#annualSalaryInput').val(); // link input variable to form input
+    
+    let result=false;
+
     console.log(firstNameInput); // log to test
     console.log(lastNameInput); // log to test
     console.log(employeeIdInput); // log to test
     console.log(jobTitleInput); // log to test
     console.log(annualSalaryInput); // log to test
 
+    
 
     event.preventDefault(); // stop browser from default clearing submit data from js
     
     $('input').val(''); // return input fields to default on click
 
-    // append to create new line from user inputs 
-    $('tbody').append(`
-        <tr>
-        <td>${firstNameInput}</td>
-        <td>${lastNameInput}</td>
-        <td>${employeeIdInput}</td>
-        <td>${jobTitleInput}</td>
-        <td>${annualSalaryInput}</td>
-        <td><button class="deleteMe">❌🍖❌</button></td>
-        </tr>`) // end append command
+    
+    // append to create new line from user inputs
+    if (firstNameInput === '' || lastNameInput === ''|| employeeIdInput === '' || jobTitleInput === '' || annualSalaryInput === '') {
+        alert('You did not complete all the required fields.'); 
+    } else {
+     $('tbody').append(`
+            <tr>
+            <td>${firstNameInput}</td>
+            <td>${lastNameInput}</td>
+            <td>${employeeIdInput}</td>
+            <td>${jobTitleInput}</td>
+            <td>${annualSalaryInput}</td>
+            <td><button class="deleteMe">❌🍖❌</button></td>
+            </tr>`) // end append command
+        result=true;
+    }
+   // return result;
 } // end handleSubmit function
 
 // failed attemped function to clear inputs
